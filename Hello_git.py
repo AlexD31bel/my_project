a0401da97a65d220f84ed82cc3e1a3b92b8c46ce
Alex Danilets <Danilets@yandex.ru>
2025-06-07 12:14:18 +0300

    Add about vcs

--- Hello_git.py
+++ Hello_git.py
@@ -5,3 +5,5 @@ print('create new branch')
 for num in range(10):
     print(num, end=' ')
 print (f'\nYour current directory: {os.getcwd()}')
+s = 'Version Control System'
+print(f'{s} - length is {len(s)} characters')
