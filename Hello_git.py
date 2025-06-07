9eac6083123ff9523073e17c80d7fa50c6405b43
Alex Danilets <Danilets@yandex.ru>
2025-06-07 12:04:11 +0300

    Add os

--- Hello_git.py
+++ Hello_git.py
@@ -1,5 +1,7 @@
+import os
 print ('Hello, GIT')
 print ("Indexing is important")
 print('create new branch')
 for num in range(10):
     print(num, end=' ')
+print (f'\nYour current directory: {os.getcwd()}')
